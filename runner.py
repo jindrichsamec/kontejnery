65b32ae29e43636dfb1d9fb8f3e8ccc360c65fe3
Jindrich Samec <jindrich.samec@xjs.cz>
2016-12-11 15:27:36 +0100

    Setting PORT as env variable

--- runner.py
+++ runner.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 containers = create_app()
 
 if __name__ == '__main__':
-    containers.run(debug=True, host='0.0.0.0')
+    port = os.environ.get('PORT') or 5000
+    containers.run(host='0.0.0.0', port=int(port))
