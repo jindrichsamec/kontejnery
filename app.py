b33c609ce3422f4bdd9584786987e1884da4cb64
Jindrich Samec <jindrich.samec@xjs.cz>
2016-12-11 14:55:09 +0100

    Database configuration

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from database import register_db
 from bootstrap import register_bootstrap
 from containers import register_container
 from containers.admin import register_admin
+import os
 
 
 def create_app():
   app = Flask(__name__)
   app.config['SECRET_KEY'] = 'd76b9af7-0caf-4749-b671-65912beea187'
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://localhost:5432/containers'
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'postgresql+psycopg2://localhost:5432/containers'
   app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
   app.config['MAPBOX_MAP_ID'] = 'mapbox.streets'
