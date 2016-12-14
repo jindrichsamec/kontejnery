2771677419f31ca77c65a36a936163d6e83934fc
Jindrich Samec <jindrich.samec@xjs.cz>
2016-12-14 21:59:22 +0100

    Shows map on the page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import GoogleMap from 'google-map-react';
 import './App.css';
+import Container from './Container'
 
-class App extends Component {
+export default class App extends Component {
+
+  static defaultProps = {
+    defaultCenter: {lat: 50.1197589, lng: 14.4673313},
+    defaultZoom: 14,
+    apiKey: 'AIzaSyC8NUe06Wy66Vf-4-tTIzobuGm4GXIdBDI',
+  }
+
   render() {
+    const { defaultCenter, defaultZoom, apiKey } = this.props;
+    const bootstrapURLKeys = {
+      key: apiKey,
+    };
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <GoogleMap
+        bootstrapURLKeys={bootstrapURLKeys}
+        defaultCenter={defaultCenter}
+        defaultZoom={defaultZoom} >
+        <Container />
+      </GoogleMap>
     );
   }
 }
-
-export default App;
