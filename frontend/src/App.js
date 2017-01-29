e3b81b9390128ac6abac32ad22172d9bffcb6bf6
Jindrich Samec <jindrich.samec@xjs.cz>
2017-01-29 23:03:45 +0100

    Layout based on BootLeaf template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,12 +10,14 @@ import Container from './Container'
 import Controller from './Controller'
 import Center from './Center'
 
+
+
 export default observer(class App extends Component {
 
   static defaultProps = {
     defaultCenter: {lat: 50.1197589, lng: 14.4673313},
     defaultZoom: 14,
-    apiKey: 'AIzaSyC8NUe06Wy66Vf-4-tTIzobuGm4GXIdBDI',
+    apiKey: process.env.GOOGLE_MAPS_API_KEY || 'AIzaSyC8NUe06Wy66Vf-4-tTIzobuGm4GXIdBDI',
   }
 
   obState = observable({
@@ -39,20 +41,22 @@ export default observer(class App extends Component {
     const { center } = this.obState;
 
     return (
-      <div>
+      <span>
         <Controller onSearch={this._handleSearch} onLocate={this._handleLocate} />
-        <div className="map">
-          <GoogleMap
-            bootstrapURLKeys={bootstrapURLKeys}
-            defaultCenter={defaultCenter}
-            defaultZoom={defaultZoom}
-            hoverDistance={24}
-            center={center}>
-            {center ? <Center {...center} /> : null}
-            {this.obState.containers.map((container) => <Container {...container} {...container.coordinates} key={container.id} />)}
-          </GoogleMap>
+        <div id="container">
+          <div id="map">
+            <GoogleMap
+              bootstrapURLKeys={bootstrapURLKeys}
+              defaultCenter={defaultCenter}
+              defaultZoom={defaultZoom}
+              hoverDistance={24}
+              center={center}>
+              {center ? <Center {...center} /> : null}
+              {this.obState.containers.map((container) => <Container {...container} {...container.coordinates} key={container.id} />)}
+            </GoogleMap>
+          </div>
         </div>
-      </div>
+      </span>
     );
   }
 })
--- frontend/src/Controller.jsx
+++ frontend/src/Controller.jsx
@@ -3,10 +3,28 @@ import SearchForm from './SearchForm'
 import GeoLocateButton from './GeoLocateButton'
 
 const Controller = ({ onSearch, onLocate }) => {
+
   return(
-    <div className="card card-block controller">
-      <SearchForm onSearch={onSearch}/>
-      <GeoLocateButton onLocate={onLocate}/>
+     <div className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <div className="navbar-icon-container">
+            <a href="#" className="navbar-icon pull-right visible-xs" id="nav-btn"><i className="fa fa-bars fa-lg white"></i></a>
+            <a href="#" className="navbar-icon pull-right visible-xs" id="sidebar-toggle-btn"><i className="fa fa-search fa-lg white"></i></a>
+          </div>
+          <a className="navbar-brand" href="#">Velkoobjemáky</a>
+        </div>
+        <div className="navbar-collapse">
+          <form className="navbar-form navbar-right" role="search">
+            <SearchForm onSearch={onSearch}/>
+            <GeoLocateButton onLocate={onLocate}/>
+          </form>
+          <ul className="nav navbar-nav">
+            <li><a href="#" data-toggle="collapse" data-target=".navbar-collapse.in" id="about-btn"><i className="fa fa-question-circle white"></i>&nbsp;&nbsp;About</a></li>
+            <li className="hidden-xs"><a href="#" data-toggle="collapse" data-target=".navbar-collapse.in" id="list-btn"><i className="fa fa-list white"></i>&nbsp;&nbsp;POI List</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
