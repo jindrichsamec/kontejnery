7b2b764da82e16b78c137a0a280adbb42ebac45b
Jindrich Samec <jindrich.samec@xjs.cz>
2017-06-12 21:50:04 +0200

    prop-types as separated node module

--- frontend/src/Container/index.jsx
+++ frontend/src/Container/index.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { observable } from 'mobx'
 import { observer } from 'mobx-react'
 import Icon from '../Icon'
